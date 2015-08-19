6d9815aecc4068d9c053ec9b33ebce21b33fddb0
Sailor Socks <socksster@gmail.com>
2015-08-18 20:56:26 -0400

    Fix while statement; math expression stated

--- metrocard/metroCard_TESTING.py
+++ metrocard/metroCard_TESTING.py
@@ -22,7 +22,7 @@ new_rides_remainder = int(100 * (add_amt % single_fare))
 
 #	> > > > > > 		SECTION 1	to "if" - Queries: current amt, addl to add, # rides, will there be remainder of $$$?
 #	> > > > > > 		SECTION 2	"else" - if remainder $$$, go to metroplus to figure out even amts to add for 0 remainder
-	
+
 
 def metrocard():
 	print orig
@@ -34,16 +34,71 @@ def metrocard():
 	else:
 		print "but you will have %.2f remaining on your card - go to metroplus()" % remainder
 		metroplus()
-	
 
+import math
 def metroplus():
 	add_amt = float(addl)
-	while (remainder != 0) and (add_amt < 100.00) :
+	while (remainder != 0) and (add_amt <= (rides + 2.75)):
 		print "New amount to add is %.2f" % add_amt
 		print "Remainder is ", remainder
 		add_amt = add_amt + .05
-
 	print "We're done here!"	
+
+#	START HERE !!!
+# if ORIG_AMT <= 2.75:
+#	RIDES_ADDING			--> # rides being added: BONUS (add amt + 11%) / 2.75, round to nearest .05
+#	+ 1 (to round up for amt originally on card)
+# else:
+#	RIDES_CURRENT + RIDES_ADDING			
+
+		
+		
+#	> > > > > > 		THE MATH
+
+			
+	
+# FORMULA - where:
+# x = amt to add
+# y = amt on card
+# 	(x + (x * .11) + y) / 2.75 = 0
+
+
+#	> > > > > > 		END OF THE MATH
+
+
+
+
+#			FROM ORIGINAL FILE (seems to work for getting floor & ceiling)
+#			COMMENTED FOR TESTING FOR MATH
+
+"""
+# math - floor & ceil:
+# CLOSEST number BELOW num or expression divided by divisor
+# math.floor(a-number-or-expression)  -  in this case, looking for amt to add divided by 2.75
+
+import math
+def addl_value_choices():
+	print cust_pref
+	pref_plus_bonus = cust_pref + (cust_pref * .11)
+	total_actual = pref_plus_bonus + card_value
+	y = pref_plus_bonus
+	t = y + z
+	if total_actual % 2.75 == 0:
+		print "Go ahead - the final total will give you exactly %d" % (total_actual / 2.75),
+		print "rides"
+	else:
+#				
+		lower = total_actual - math.floor(total_actual / 2.75)
+		upper = total_actual + math.ceil(total_actual / 2.75)
+		
+		print "You can either add %0.2f" % lower
+		print "for %d rides" % math.floor(total_actual / 2.75)
+		print "or you can add %0.2f" % upper
+		print "for %d rides" % math.ceil(total_actual / 2.75)
+	
+#			END FROM ORIGINAL FILE
+
+"""
 
 
 
