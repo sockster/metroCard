14d43f82784c5505f0b4fb5dc3d18b657af36c35
Sailor Socks <socksster@gmail.com>
2015-08-20 20:04:28 -0400

    Force 2-decimal w/ import Decimal, getcontext

--- metrocard/SANDBOX_mc.py
+++ metrocard/SANDBOX_mc.py
@@ -22,19 +22,38 @@ def testing():
 """
 
 def testing():
-	x = raw_input("on card now\n")			# to be fixed amt
-	y = raw_input("to add to card\n")		# to be var, added to by .05 until div by 2.75
+	from decimal import Decimal, getcontext
+	getcontext().prec = 2
 
-	x = float(x)
-	y = float(y)
+	x = Decimal(raw_input("on card now\n"))			# to be fixed amt
+	y = float(raw_input("to add to card\n"))		# to be var, added to by .05 until div by 2.75
+	print "Decimal(x)", Decimal(x)
+
+	# convert float to 2-decimal #
+	print 'x = "{:.2f}".format(x)'
+	x = "{:.2f}".format(x)
+	y = "{:.2f}".format(y)
 	z = 0
+	
+	print "x"
+	print x
+	print y
+	
+
+
+
+
+
+	
+"""	
 #	while z <= 10:
-	if "{:.2f}".format((y + (y * .11) + x) / 2.75) != 0:
+	if (y + (y * .11) + x) / 2.75 != 0:
+#	if "{:.2f}".format((y + (y * .11) + x) / 2.75) != 0:
 		y += .05
 #		z += 1
 	else:
 		print "outta' heah!"
-	print "{:.2f}".format(x), "This is x"
+	print x, "This is x"
 	print "{:.2f}".format(y), "This is y"
 	print "{:.2f}".format(z), "This is z"
 
@@ -43,6 +62,9 @@ def testing():
 #	 .80 as x
 #	7.80 as y
 #	0.00 as z
+"""
+
+
 
 testing()
 
