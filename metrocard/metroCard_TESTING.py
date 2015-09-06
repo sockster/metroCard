03903988116cf0da427a409261e78526a129efd9
Sailor Socks <socksster@gmail.com>
2015-09-06 14:41:23 -0400

    Figure math for add'l, bonus and total

--- metrocard/metroCard_TESTING.py
+++ metrocard/metroCard_TESTING.py
@@ -32,26 +32,12 @@ def metrocard():
 	print addl
 	print "your total - including original amount and 11 percent bonus will be %.2f" % total
 	print "you will have %i rides" % rides
-#	remainder = total % single_fare
 
 	if rides_remainder == 0:
 		print "this amount will leave you with $0 on your metrocard!"		# END OF SECTION 1
 	else:
 		print "but you will have $%.2f remaining on your card - go to metroplus()" % remainder
 		metroplus()
-
-
-
-
-
-# Do the vars get passed from metrocard() to metroplus()?  	YES
-#################### NEXT STEPS: (save each var as 2-decimal num)
-#	Do the math for floor & ceil:
-#	Create a list of 2.75 multiples (using recursive multiplication)
-#	Compare total to list for match (index-match?)
-#	If not found, subtract .05 from addl_amt, save as var_less and try until match found
-#	Now ADD .05 to addl_amt, save as var_more and try until match found
-#	Print original total w/remainder amt, and offer floor and ceil amts for zero remainder on card
 
 
 
@@ -69,9 +55,8 @@ def metroplus():
 
 
 
-
-metrocard()	#what's on YOUR card?	|	with add'l amt, you'll have:	|	find the closest 0-remainder total
-
+#	> > > > > > 		SECTION 1a		BREAK from FORMULA; create list containing multiples of 2.75
+metrocard()
 #	show 50 multiples of 2.75	
 multis = []
 x = 1.00
@@ -80,30 +65,59 @@ while x < 50:	# 50 for testing purposes only
 	multis.append(y)
 	x += 1
 print multis
-	
+print add_amt
 
 
+#	> > > > > > 		SECTION 2	BACK to FORMULA; BONUS AND TOTAL NOT WORKING ...
+#	 		find the closest 0-remainder total
 
-#	> > > > > > 		SECTION 2	"else" - if remainder $$$, figure out even amts to add for 0 remainder
-
-
-#	BONUS AND TOTAL NOT WORKING ... but we're getting closer
 def finding_nemo():
-	add_amt = float(addl)
-#	while (remainder != 0) and (add_amt < 50):
-	while add_amt < 50:
+	print "Finding Nemo"
+	print add_amt
+	print "Nemo found!"	
+	chasing_dory()
+
+
+
+def chasing_dory():
+	print "Chasing Dory"
+	print add_amt
+	#	re-calcs for pertinent vars
+	new_add = add_amt
+	new_remainder = total % 2.75
+	
+	while new_add < 20 and new_remainder != 0:
+		new_add = new_add + .05
+		new_bonus_only = new_add * .11
+		new_bonus = new_add + new_bonus_only
+		new_total = orig_amt + new_bonus
+		new_rides = new_total / 2.75
+		new_remainder = round((new_total % 2.75) / .05) * .05
+
+		print "Print new_add", new_add
+		print "Print new_bonus_only", new_bonus_only
+		print "Rounded & divided  -->", round(new_bonus_only / .05) * .05
+		print "Print new_bonus - rounded", round(new_bonus / .05) * .05
+		print "Print new_total - rounded", round(new_total / .05) * .05
+		print "Print new_rides - rounded", round(new_rides / .05) * .05
+		print "Print new_remainder - rounded", round(new_remainder / .05) * .05
+		print ""
+		
+	print "Dory caught!"	
+
+"""	while add_amt < 50:
 		print "New amount to add is %.2f" % add_amt	# CORRECT
 		print "Remainder is ", remainder
 		print "Print add_amt:", add_amt
-		add_amt = add_amt + .05
-
 		print "Print single_fare:", single_fare		# CORRECT
+		print "Print bonus_only", bonus_only 
 		print "Print bonus:", bonus					# CORRECT
 		print "Print total:", total					# CORRECT
 		print "Remainder is ", remainder
-
-	print "We're done here!"	
-
+		print ""
+		print ""
+		add_amt = add_amt + .05
+"""
 
 
 finding_nemo()
@@ -112,146 +126,4 @@ finding_nemo()
 
 
 
-
-
-
-
-"""
-#			FROM ORIGINAL FILE (seems to work for getting floor & ceiling)
-#			COMMENTED FOR TESTING FOR MATH
-
-# math - floor & ceil:
-# CLOSEST number BELOW num or expression divided by divisor
-# math.floor(a-number-or-expression)  -  in this case, looking for amt to add divided by 2.75
-
-import math					# 	math MOD NEEDED?
-def addl_value_query():	#	IF TOTAL AMT IS DIVISIBLE BY 2.75
-	print orig_amt
-	print add_amt
-	bonus_new = add_amt + (add_amt * .11)
-	total_new = bonus_new + orig_amt
-	if total % 2.75 == 0:
-		print "Go ahead - the final total will give you exactly %d rides" % (total / 2.75)
-
-	else:
-		total_new = total
-		#	L = [expression for variable in sequence]
-		L = [total_new == for total_new in multis]
-		print "Print L"
-		print L
-
-addl_value_query()
-
-"""	
 	
-	
-	
-"""	
-	
-#				
-		lower = total_actual - math.floor(total_actual / 2.75)
-		upper = total_actual + math.ceil(total_actual / 2.75)
-		
-		print "You can either add %0.2f" % lower
-		print "for %d rides" % math.floor(total_actual / 2.75)
-		print "or you can add %0.2f" % upper
-		print "for %d rides" % math.ceil(total_actual / 2.75)
-	
-#			END FROM ORIGINAL FILE
-
-
-addl_value_query()
-"""
-
-
-"""
-#	OK - CAN'T FIGURE OUT H/T GET TO 0 MANUALLY, LET'S TRY MATCH AND RECURSIVE GUESSING
-#	SAMPLE ... FOR POSSIBLE USE LATER
-import re
-def metromatch():
-	y = total
-	m = re.search(y, multis)
-	if m:
-		print ("search", m.group(1))
-"""
-
-
-
-"""
-#	START HERE !!!
-# if ORIG_AMT <= 2.75:
-#	RIDES_ADDING			--> # rides being added: BONUS (add amt + 11%) / 2.75, round to nearest .05
-#	+ 1 (to round up for amt originally on card)
-# else:
-#	RIDES_CURRENT + RIDES_ADDING			
-
-
-
-#			FROM ORIGINAL FILE (seems to work for getting floor & ceiling)
-#			COMMENTED FOR TESTING FOR MATH
-
-# math - floor & ceil:
-# CLOSEST number BELOW num or expression divided by divisor
-# math.floor(a-number-or-expression)  -  in this case, looking for amt to add divided by 2.75
-
-import math					# 	math MOD NEEDED?
-def addl_value_query():	#	IF TOTAL AMT IS DIVISIBLE BY 2.75
-	print cust_pref
-	pref_plus_bonus = cust_pref + (cust_pref * .11)
-	total_actual = pref_plus_bonus + card_value
-	y = pref_plus_bonus
-	t = y + z
-	if total_actual % 2.75 == 0:
-		print "Go ahead - the final total will give you exactly %d" % (total_actual / 2.75),
-		print "rides"
-	else:
-	
-	
-	
-	
-	
-#				
-		lower = total_actual - math.floor(total_actual / 2.75)
-		upper = total_actual + math.ceil(total_actual / 2.75)
-		
-		print "You can either add %0.2f" % lower
-		print "for %d rides" % math.floor(total_actual / 2.75)
-		print "or you can add %0.2f" % upper
-		print "for %d rides" % math.ceil(total_actual / 2.75)
-	
-#			END FROM ORIGINAL FILE
-
-
-
-# var_statmts - THE NEW SHIT ... kickin' it old school (sep raw_input stmt from ans)
-
-#	var_statements: define the vars and do the initial %age math
-def var_statemts():
-	print "on card now"
-	x = float(raw_input())			# on card now amt
-	print "to add to card"
-	y = float(raw_input())			# adding amt
-	y2 = y + round((y * .11),2)		# adding amt + 11% bonus
-	print "Print y2:", y2
-	print ("{:.2f}".format(y2))
-	z = x + y2
-	print "Print z:", z
-	print ("{:.2f}".format(y2))
-
-	08.23.15	Done; Next; Waiting:
-	D - Ask how much on card - save as var
-	D - Ask how much to add - save as var
-	D - Add 11% bonus to add amt - save as var
-	D - Add to orig amt on card - save as var
-	D - Divide by fare ($2.75)
-	D - Return number of rides
-	D - Return amt left on card
-	D - If amt != 0, go to metroplus() for...
-	N - If amt < 2.75
-"""	
-	
-#var_statemts()
-
-#def metromatch2():
-	
-#metromatch()
