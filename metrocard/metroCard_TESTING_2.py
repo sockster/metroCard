a35d1731c8323e12ecec241398a4f3a639664b59
Sailor Socks <socksster@gmail.com>
2015-09-21 13:44:24 -0400

    Check and complete code for Current Amt and Rides on card

--- metrocard/metroCard_TESTING_2.py
+++ metrocard/metroCard_TESTING_2.py
@@ -3,6 +3,9 @@ card_value = 0
 
 
 
+
+
+
 #	> > > > > > 		SECTION 1
 #							What is on card now: - money
 #												 - # of rides
@@ -17,15 +20,33 @@ def card_status():
 
 	elif (new_card == "N") or (new_card == "n"):
 		orig_amt = float(raw_input("What is the value on your card now?\n"))
-		print "print orig_amt", orig_amt				# TESTING ONLY - T/B DELE
+		print "print orig_amt", round(orig_amt,2)		# TESTING ONLY - T/B DELE
 		rides = (orig_amt / 2.75)
+		rides = round(rides,2)
 		print "print rides", rides						# TESTING ONLY - T/B DELE
-		print "Right now your card has %0.2f rides remaining" % rides
+		print "Print type", type(rides)					# TESTING ONLY - T/B DELE
+		print "Right now your card has %s rides remaining" % int(rides)
 
 	else:
 		print "Please enter \"Y\" or \"N\" \n"
 		card_status()
 
+# # # # # # # # # # # GOOD TO HERE # # # # # # # # # # # 
+
+
+
+
+
+#	> > > > > > 		SECTION 2
+#							What to add: 	- money
+#											- # of rides
+
+# > > > > NEXT STEPS:
+#		How much do you want to add?
+#		  - check if div by $0.05
+#		  - if < $5.50, no bonus, i.e., straight addition
+#		  - else addl 11% bonus to amt adding
+#		Return total of card after amt added, and # of rides
 
 
 
@@ -33,9 +54,10 @@ def card_status():
 
 
 
+if __name__ == "__main__":
+	card_status()
 
-card_status()
-
+	addl_amt = raw_input("How much do you want to add?\n")
 
 
 
